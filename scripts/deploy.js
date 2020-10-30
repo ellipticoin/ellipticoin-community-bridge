714498f4e71404ef72ffa89b0f9cb9223dfe9647
Mason Fischer <mason@kissr.co>
2020-10-30 15:01:55 -0400

    Use mainnnet WETH in deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const {formatUnits} = ethers.utils
 async function main() {
   console.log((await (await ethers.getSigners())[0].getGasPrice()).toNumber())
   const Bridge = await ethers.getContractFactory("Bridge");
-  const bridge = await Bridge.deploy("0xd0a1e359811322d97991e03f863a0c30c2cf029c");
+  const bridge = await Bridge.deploy("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
   let tx = await bridge.deployed();
 
   console.log("Bridge deployed to:", bridge.address);
