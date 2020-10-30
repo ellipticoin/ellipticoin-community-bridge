8487967506ea00db3369845ff2f50b8444c5a048
Mason Fischer <mason@kissr.co>
2020-10-30 15:51:15 -0400

    Add back hardhat.config.prod.js

---
+++ hardhat.config.prod.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.getAddress());
+  }
+});
+
+module.exports = {
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ETH_PRIVATE_KEY}`],
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ETH_PRIVATE_KEY}`],
+    },
+  },
+  solc: {
+    version: "0.6.12",
+  },
+};
